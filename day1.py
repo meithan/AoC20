f5e96d93ab1c5bf6b1e074d2c4f259c4335be9b1
Meithan West <meithan@gmail.com>
2020-12-01 00:41:48 -0600

    Day 1 solution

---
+++ day1.py
@@ -0,0 +1,36 @@
+# Day 1: Report Repair
+
+import sys
+
+# =================================================
+
+# Read input
+entries = []
+with open(sys.argv[1]) as f:
+  for line in f:
+    entries.append(int(line.strip()))
+
+# -------------------------------------------------
+
+# Part 1
+
+for x in entries:
+  for y in entries[1:]:
+    if x + y == 2020:
+      sol1 = x * y
+      break
+
+print("Part 1:", sol1)
+
+# -------------------------------------------------
+
+# Part 2
+
+for x in entries:
+  for y in entries[1:]:
+    for z in entries[2:]:
+      if x + y + z == 2020:
+        sol2 = x * y * z
+        break
+
+print("Part 2:", sol2)
