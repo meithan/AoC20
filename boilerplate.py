2d27a3ca50329969e4013233a88dda6659941e7f
Meithan West <meithan@gmail.com>
2020-12-03 23:37:57 -0600

    Add boilerplate code

---
+++ boilerplate.py
@@ -0,0 +1,20 @@
+#
+
+import sys
+
+# ------------------------------------------------------------------------------
+
+if len(sys.argv) == 1:
+  sys.argv.append(sys.argv[0].replace(".py", ".in"))
+
+with open(sys.argv[1]) as f:
+  for line in f:
+    pass
+
+
+
+print("Part 1:")
+
+
+
+print("Part 2:")
